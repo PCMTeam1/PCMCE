690a4e68ddd0ae44974ea8f20c5e4f7cb4fd72cf
yvesmocq <yves.mocquard.1@etudiant.univ.rennes1.fr>
2014-07-02 10:24:24 +0200

    Almost last version of Matrix

--- Project_PCMCE/src/fr/istic/dugl/pcmce/PCMReader/MatrixImplDUGL.java
+++ Project_PCMCE/src/fr/istic/dugl/pcmce/PCMReader/MatrixImplDUGL.java
@@ -1,41 +1,20 @@
 package fr.istic.dugl.pcmce.PCMReader;
 
-/**
- * @author Yves Le Monnier
- *
- */
-
-package fr.istic.dugl.pcmce.PCMReader;
-
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.logging.Logger;
-import java.util.logging.Logger;
 
 import pcmmm.Cell;
 import pcmmm.Matrix;
 
 public class MatrixImplDUGL implements IMatrix {
 	
-	public static Logger _LOGGER = Logger.getLogger("MatrixImplDUGL");
 	private int id;
 	private String name;
 	
-	private int nbAllRows;
-	private int nbAllColumns;
-	
-	private int nbHeadersProductRows;
-	private int nbHeadersProductColumns;
-	
-	private int nbHeadersFeatureRows;
-	private int nbHeadersFeatureColumns;
-	
-	private int nbExtraRows;
-	private int nbExtraColumns;
-	
+	private int nbRows;
+	private int nbColumns;
 	
 	private List<ICell> listAllCells;
 	private ICell[][] tabAllCells;
@@ -52,56 +31,34 @@ public class MatrixImplDUGL implements IMatrix {
 	private List<ICell> listExtraCells;
 	private ICell[][] tabExtraCells;
 	
-	private SortedSet<Integer> featureIndices;
-	private SortedSet<Integer> productIndices;
-	
 	public MatrixImplDUGL(Matrix matrix){
-		listAllCells = new ArrayList<ICell>();
+		listAllCells = new LinkedList<ICell>();
 		
-		listValuedCells = new ArrayList<ICell>();
+		listValuedCells = new LinkedList<ICell>();
 		
-		listHeaderProductCells = new ArrayList<ICell>();
+		listHeaderProductCells = new LinkedList<ICell>();
 		
-		listHeaderFeatureCells = new ArrayList<ICell>();
+		listHeaderFeatureCells = new LinkedList<ICell>();
 		
-		listExtraCells = new ArrayList<ICell>();
+		listExtraCells = new LinkedList<ICell>();
 		
 		//Compute number of rows and numbers of columns
-		int column,row;
-		nbAllRows = 0;
-		nbAllColumns = 0;
 		for(Cell cell : matrix.getCells()){
-			column = cell.getColumn();
-			row =  cell.getRow();
-			if(row>nbAllRows) {nbAllRows=row;}
-			if(column>nbAllColumns) {nbAllColumns=column;}
+			int column = cell.getColumn();
+			int row =  cell.getRow();
+			if(row>nbRows) {nbRows=row;}
+			if(column>nbColumns) {nbColumns=column;}
+			
+			ICell myCellDUGL = new CellImplDUGL(cell);
+			listAllCells.add(myCellDUGL);
 		}
-		nbAllRows++; nbAllColumns++;
+		nbRows++; nbColumns++;
 		
-		tabAllCells = new ICell[nbAllRows][nbAllColumns];
-
+		tabAllCells = new ICell[nbRows][nbColumns];
 		
 		for(Cell cell : matrix.getCells()){
-			column = cell.getColumn();
-			row =  cell.getRow();
-			ICell myCellImplDUGL = new CellImplDUGL(cell);
-			listAllCells.add(myCellImplDUGL);
-			tabAllCells[row][column] = myCellImplDUGL;
-			if(myCellImplDUGL.isHeaderFeature()){
-				listHeaderFeatureCells.add(myCellImplDUGL);
-			}
-			else if(myCellImplDUGL.isHeaderProduct()){
-				listHeaderProductCells.add(myCellImplDUGL);
-			}
-			else if(myCellImplDUGL.isExtra()){
-				listExtraCells.add(myCellImplDUGL);
-			}
-			else if(myCellImplDUGL.isValued()){
-				listValuedCells.add(myCellImplDUGL);
-			}
-			else{
-				_LOGGER.info("Type of Cell not supported");
-			}
+			int column = cell.getColumn();
+			int row =  cell.getRow();
 		}
 		
 	}
@@ -235,25 +192,21 @@ public class MatrixImplDUGL implements IMatrix {
 		return result;
 	}
 	
-	public  SortedSet<Integer> getFeatureIndices()
+	public  SortedSet<Integer> getAllIndicesFeature()
 	{
-		return featureIndices;
-	}
-	public  SortedSet<Integer> getProductIndices()
-	{
-		return productIndices;
+		
 	}
 	
 	
 	@Override
 	public DetailsOfCells getDetailsOfCellsFromFilters(List<IFilter> listFilters)
 	{
-		SortedSet<Integer> productIndicesSet=getProductIndices();
-		SortedSet<Integer> featureIndicesSet=getFeatureIndices();
+		SortedSet<Integer> productIndicesSet=null;
+		SortedSet<Integer> featureIndicesSet=new TreeSet<Integer>();
 			
 		for( IFilter filter:listFilters )
 		{
-			SortedSet<Integer> resultFiltre = filter.getIndices( this );
+			SortedSet<Integer> resultFiltre = filter.getIndices();
 			if ( filter.getTypeFilter() == IFilter.TypeFilter.TypeFilterProduct )
 			{
 				productIndicesSet = intersection( productIndicesSet, resultFiltre );
